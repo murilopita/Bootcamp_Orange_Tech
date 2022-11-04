59a6cf1669db4db9f439f5aa15e2c28ea5a73caf
Murilo Pita <murilo.junior95@hotmail.com>
2022-11-04 00:05:15 -0300

    Aula 2 Completa

---
+++ Bootcamp/Modulo_03/Aula_02/2_Estruturas_Condicionais.js
@@ -0,0 +1,14 @@
+//Estruturas Condicionais
+
+const numero = 10;
+const numeroDivisivelPor5 = (numero % 5) === 0;
+
+if (numero === 0) {
+    console.log('O número é Inválido');
+} 
+
+else if (numeroDivisivelPor5) {
+    console.log('Sim');
+} else {
+    console.log('Não');
+}
