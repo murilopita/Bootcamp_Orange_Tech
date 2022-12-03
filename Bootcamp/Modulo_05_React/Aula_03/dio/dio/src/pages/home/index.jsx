86202cc3891f8fdeae46f7354ea1307fc38f1398
Murilo Pita <murilo.junior95@hotmail.com>
2022-12-02 23:47:12 -0300

    Clone Dio -  Button, Header, Page Home, complete

--- Bootcamp/Modulo_05_React/Aula_03/dio/dio/src/pages/home/index.jsx
+++ Bootcamp/Modulo_05_React/Aula_03/dio/dio/src/pages/home/index.jsx
@@ -1,9 +1,40 @@
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import bannerImage from '../../assets/banner.png'
+
+import { 
+    Container,
+    TextContent,
+    Title,
+    TitleHighlight
+
+} from './styles';
+
 
 const Home = () => {
     return (<>
-        <h1>Home</h1>
-        <Link to="/login">Fazer Login</Link>
+        <Header />
+        <Container>
+            <div>
+            <Title>
+                <TitleHighlight>
+                    Implemente <br />
+                </TitleHighlight>
+                o seu futuro global agora!
+            </Title>
+                
+            <TextContent>
+                Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare seu novo desafio profissional, evoluindo em comunidade com os melhores experts.
+            </TextContent>
+            
+            <Button title="Começar agora" variant="secondary" />
+            </div>
+            <div>
+                <img src={bannerImage} alt="Imagem principal" />
+            </div>
+        </Container>
+        
     </>)
 }
 
