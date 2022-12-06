56f613fb475f60f28be496861265313b8e2ad3c0
Murilo Pita <murilo.junior95@hotmail.com>
2022-12-05 23:13:26 -0300

    React Hook Forms complete

--- Bootcamp/Modulo_05_React/Aula_03/dio/dio/src/pages/login/index.jsx
+++ Bootcamp/Modulo_05_React/Aula_03/dio/dio/src/pages/login/index.jsx
@@ -1,9 +1,15 @@
 import { MdEmail, MdLock } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
+
+import { api } from '../../services/api'
+
 import { 
     Column,
     Container,
@@ -17,14 +23,36 @@ import {
 
 } from './styles';
 
+const schema = yup.object({
+    email: yup.string().email('email não é válido').required('Campo Obrigatório'),
+    password: yup.string().min(3, 'No mínimo 3 caracteres').required('Campo Obrigatório'),
+    }).required();
+
+
 
 const Login = () => {
 
     const navigate = useNavigate();
 
-    const handleClickSignIn = () => {
-        navigate('/feed')
-    }
+    const { control, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+        mode: 'onChange',
+    });
+
+    const onSubmit = async formData => {
+        try{
+            const { data } = await api.get(`users?email=${formData.email}&senha=${formData.password}`);
+            if(data.length === 1){
+                navigate('/feed')
+            } else {
+                alert('Email ou Senha inválido')
+            }
+        }catch {
+            alert('Houve um erro, tente novamente.')
+        }
+
+    };
+    
     return (<>
         <Header />
         <Container>
@@ -37,10 +65,11 @@ const Login = () => {
                 <Wrapper>
                     <TitleLogin>Faça seu Cadastro</TitleLogin>
                     <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
-                    <form>
-                        <Input placeholder='Email' leftIcon={<MdEmail />} />
-                        <Input placeholder='Senha' type='password' leftIcon={<MdLock />} />
-                        <Button title='Entrar' variant='secondary' onClick={handleClickSignIn} type='button'/>
+                    <form onSubmit={handleSubmit(onSubmit)} >
+                        <Input name="email" errorMessage={errors?.email?.message} control={control} placeholder='Email' leftIcon={<MdEmail />} />
+                        <Input name="password" control={control} placeholder='Senha' type='password' errorMessage={errors?.password?.message} leftIcon={<MdLock />} 
+                        />
+                        <Button title='Entrar' variant='secondary' type='submit'/>
                     </form>
                     <Row>
                         <EsqueciText>Esqueci minha senha</EsqueciText>
